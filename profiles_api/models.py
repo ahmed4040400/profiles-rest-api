716e584d5525bec33d7da8bb995a9141397b3726
the variable <ahmed4040400@gmail.com>
2020-02-06 19:57:29 +0200

    added custom user profile , manager

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import  BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """manage the user profile
+       like adding users and super users
+     """
+
+    def creat_user(self , email,name,password=None):
+
+        """creat a normal user"""
+        if not email :
+            raise ValueError("the user must have an email")
+        # to make the second half of the email not case senstive
+        email = self.normalize_email(email)
+        #adding the name and the emails to the model
+        #which iin this case is the UserProfile model
+        user = seld.model(email = email , name = name)
+        #adding the password to the model using the
+        #set_password method
+        user.set_password(password)
+        #saving the user object and specify the using is optional
+        #incase we're using multiple dbs
+        user.save(using = self._db)
+        #returns the user object
+        return user
+
+    def creat_super_user(self , email,name,password):
+        """for creating a super user"""
+        # start by creating a normal user
+        user = self.creat_user(email,name,password)
+        #then add the required permissions to be a super user
+        # note : the supper user permissions is automaticly created
+        #the the PermissionsMixin
+        user.is_superuser = True
+        user.is_staff = True
+        #and then save the user
+        user.save(using = self._db)
+        #and then return the user
+        return user
+
+
+
+
+class UserProfile(AbstractBaseUser , PermissionsMixin):
+    """database model for user in the system"""
+
+    email = models.EmailField(max_length = 255 ,unique = True )
+    name = models.CharField(max_length= 255)
+    is_active = models.BooleanField(default = True)
+    #is admin
+    is_staff = models.BooleanField(default = False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FILEDS = ['name']
+
+    def get_full_name(self):
+        #TODO:returns the user full name
+        return self.name
+
+    def get_short_name(self):
+        #TODO:returns the user short name
+        return self.name
+
+    #for preview the users based on the email adress
+    def __str__(self):
+        return self.email
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'profiles_api',
 ]
 
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# teeling django to use this model as
+#the main model for authentication stuff
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
