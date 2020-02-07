74ed042ab91fb94fe36388ca87cbbbbb52aed3ce
the variable <ahmed4040400@gmail.com>
2020-02-07 18:56:16 +0200

    adding more comments

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from profiles_api import views
 
+#an other url pathes under the parent url urlpattern wich is api/
+
+
 urlpatterns = [
+    #as_view() is a method from the APIView class
+    #that we inherited in our helloWorld class
+    # that allow django to know that this is ment to be
+    # an api view 
     path('hello/', views.helloWorld.as_view()),
 ]
