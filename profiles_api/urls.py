11ef7badeb062001f4f2f2170eda16a2881777ec
the variable <ahmed4040400@gmail.com>
2020-02-11 20:56:27 +0200

    added viewsets to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,13 +1,20 @@
-from django.urls import path
+from django.urls import path,include
 from profiles_api import views
 
+from rest_framework.routers import DefaultRouter
 #an other url pathes under the parent url urlpattern wich is api/
 
+
+router = DefaultRouter()
+router.register("helloViewSet",views.helloViewSet,base_name = "helloViewSet")
 
 urlpatterns = [
     #as_view() is a method from the APIView class
     #that we inherited in our helloWorld class
     # that allow django to know that this is ment to be
-    # an api view 
+    # an api view
     path('hello/', views.helloWorld.as_view()),
+
+    path('',include(router.urls)),
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,11 @@
-#APIView is a view to api callin
+#APIView is a view to api calling
 #any api serving class should inherit it
 from rest_framework.views import APIView
+
+#viewsets is view to also api calling but in much simpler way
+#ant basic api serving class should inherit it
+from rest_framework import viewsets
+
 #this import serves the responsing
 #it return json format as default
 from rest_framework.response import Response
@@ -40,13 +45,65 @@ class helloWorld(APIView):
             return Response (serializer.errors, status = 400)
     # in case the request method is put
     # we just return dummy data with the method name
+    # put is for updating the whole view
     def put (self , request , pk = None):
         return Response({"method" : "put"})
     # in case the request method is patch
     # we just return dummy data with the method name
+    #patch is for updating a specifice object of the views module
     def patch (self , request , pk = None):
         return Response({"method" : "patch"})
     # in case the request method is delete
     # we just return dummy data with the method name
+    #delete is for deleting a specifice object of the views module
     def delete (self , request , pk = None):
         return Response({"method" : "delete"})
+
+
+#creating the view and inherit the viewsets
+#cause its a basic api serving class
+class helloViewSet(viewsets.ViewSet):
+
+    serializer_class = serializer.helloSerializer
+
+
+    #in case the request mthod is get
+    def list (self,req):
+        return Response({"viewsets":True})
+
+    def create (self,req):
+        # pass the data to the serializer to validate it
+        serializer = self.serializer_class(data = req.data)
+        #making decision depending on the validity
+        if serializer.is_valid():
+            # getting the valid data from the serializer
+            name = serializer.validated_data.get('name')
+            #return the hello message with status code 200 ok ,
+            return Response ({"message" : f"hello {name}"})
+        else:
+            #return the serializer eroor message with a status code  400 bad requist
+            return Response(serializer.errors , status = 400)
+
+
+    # in viewsets if the function require pk
+    # it won't be visible til we put the desired pk in the url 
+
+
+    # retreive is for getting an specific object by its pk (primary key) (id)
+    def retrieve(self , req , pk = None):
+        return Response({"dummy data" : "retrieve"})
+
+    #in case the request method is put
+    def update(self , req , pk = None):
+        return Response({"dummy data" : "update"})
+
+
+    #in case the request method is put
+    def partial_update(self , req , pk = None):
+        return Response({"dummy data" : "partial_update"})
+
+
+
+    #in case the request method is patch
+    def destroy(self , req , pk = None):
+        return Response({"dummy data" : "destroy"})
